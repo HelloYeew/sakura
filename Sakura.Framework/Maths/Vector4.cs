26d9f943dd8fc7bce41bb5f525e33e5122cfb5db
Phawit Pornwattanakul <me@helloyeew.dev>
2025-09-09 04:02:59 +0700

    Add more math type that use and use source generator

---
+++ Sakura.Framework/Maths/Vector4.cs
@@ -0,0 +1,34 @@
+// This code is part of the Sakura framework project. Licensed under the MIT License.
+// See the LICENSE file for full license text.
+
+using System.Runtime.CompilerServices;
+using SystemVector4 = System.Numerics.Vector4;
+using SilkVector4 = Silk.NET.Maths.Vector4D<float>;
+
+namespace Sakura.Framework.Maths;
+
+/// <summary>
+/// A 4D vector struct with implicit conversions to and from <see cref="System.Numerics.Vector4"/>.
+/// </summary>
+[MathStruct]
+public readonly partial struct Vector4
+{
+    public readonly SystemVector4 Value;
+
+    public float X => Value.X;
+    public float Y => Value.Y;
+    public float Z => Value.Z;
+    public float W => Value.W;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator Vector4(SystemVector4 v) => new Vector4(v.X, v.Y, v.Z, v.W);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator SystemVector4(Vector4 v) => v.Value;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator SilkVector4(Vector4 v) => new SilkVector4(v.X, v.Y, v.Z, v.W);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static implicit operator Vector4(SilkVector4 v) => new Vector4(v.X, v.Y, v.Z, v.W);
+}
