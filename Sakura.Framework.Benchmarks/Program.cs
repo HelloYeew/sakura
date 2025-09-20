0ccaedb5bff85d010bc22b63ffdcef2d84ffbf72
Phawit Pornwattanakul <me@helloyeew.dev>
2025-09-21 00:56:59 +0700

    Initialize benchmark project

---
+++ Sakura.Framework.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿// This code is part of the Sakura framework project. Licensed under the MIT License.
+// See the LICENSE file for full license text.
+
+using System;
+
+namespace Sakura.Framework.Benchmarks;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
