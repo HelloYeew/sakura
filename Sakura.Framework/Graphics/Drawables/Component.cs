59fdb73a404a67b334a3f1fe3950207cd646b8ad
Phawit Pornwattanakul <me@helloyeew.dev>
2025-09-28 01:26:06 +0700

    Add component for updatable component

---
+++ Sakura.Framework/Graphics/Drawables/Component.cs
@@ -0,0 +1,13 @@
+// This code is part of the Sakura framework project. Licensed under the MIT License.
+// See the LICENSE file for full license text.
+
+namespace Sakura.Framework.Graphics.Drawables;
+
+/// <summary>
+/// Am updatable component that need to be inserted into the draw heirarchy.
+/// This inherited all from <see cref="Drawable"/> but to make things more clear visually.
+/// </summary>
+public class Component : Drawable
+{
+
+}
