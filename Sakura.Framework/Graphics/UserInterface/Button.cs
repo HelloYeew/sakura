d459e556ab8aeaf3283180f24812673b44282d01
Phawit Pornwattanakul <me@helloyeew.dev>
2025-11-06 03:18:13 +0700

    Add clickable container as button container

---
+++ Sakura.Framework/Graphics/UserInterface/Button.cs
@@ -0,0 +1,11 @@
+// This code is part of the Sakura framework project. Licensed under the MIT License.
+// See the LICENSE file for full license text.
+
+using Sakura.Framework.Graphics.Containers;
+
+namespace Sakura.Framework.Graphics.UserInterface;
+
+public abstract class Button : ClickableContainer
+{
+
+}
