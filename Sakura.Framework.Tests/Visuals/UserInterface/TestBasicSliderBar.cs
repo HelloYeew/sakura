10a71bed422909137e641d5241910044f875f62c
Phawit Pornwattanakul <me@helloyeew.dev>
2026-05-15 09:29:54 +0700

    Add test for slider share or bind same reactive

--- Sakura.Framework.Tests/Visuals/UserInterface/TestBasicSliderBar.cs
+++ Sakura.Framework.Tests/Visuals/UserInterface/TestBasicSliderBar.cs
@@ -96,4 +96,57 @@ public class TestBasicSliderBar : ManualInputManagerTestScene
     {
         AddStep("Set slider value to 75", () => slider.Current.Value = 75f);
     }
+
+    [Test]
+    public void TestTwoSlidersSameReactive()
+    {
+        BasicSliderBar slider2 = null!;
+        SpriteText valueText2 = null!;
+
+        AddStep("Add second slider", () =>
+        {
+            TestContent.Add(slider2 = new BasicSliderBar
+            {
+                Anchor = Anchor.Centre,
+                Origin = Anchor.Centre,
+                Position = new Vector2(0, 50),
+                Size = new Vector2(200, 20),
+                MinValue = 0,
+                MaxValue = 100
+            });
+
+            TestContent.Add(valueText2 = new SpriteText
+            {
+                Anchor = Anchor.TopLeft,
+                Origin = Anchor.TopLeft,
+                Position = new Vector2(0, 40),
+                Text = "Second value: 0",
+                Color = Color.White,
+                Size = new Vector2(200, 30),
+            });
+
+            slider2.Current.ValueChanged += e => valueText2.Text = $"Second value: {e.NewValue:F1}";
+        });
+
+        AddStep("Bind second slider to first slider's reactive", () =>
+        {
+            slider2.Current.BindTo(slider.Current);
+
+        });
+
+        AddStep("Move mouse to center of first slider", () => InputManager.MoveMouseTo(slider));
+        AddStep("Click center", () => InputManager.Click(MouseButton.Left));
+        AddAssert("First slider value is roughly 50", () => Precision.AlmostEquals(slider.Current.Value, 50f, 2f));
+        AddAssert("Second slider value is roughly 50", () => Precision.AlmostEquals(slider2.Current.Value, 50f, 2f));
+        AddStep("Move mouse to 3/4 of first slider", () => InputManager.MoveMouseTo(new Vector2(slider.DrawRectangle.X + slider.DrawRectangle.Width * 0.75f, slider.DrawRectangle.Y + 10)));
+        AddStep("Click 3/4", () => InputManager.Click(MouseButton.Left));
+        AddAssert("First slider value is roughly 75", () => Precision.AlmostEquals(slider.Current.Value, 75f, 2f));
+        AddAssert("Second slider value is roughly 75", () => Precision.AlmostEquals(slider2.Current.Value, 75f, 2f));
+
+        AddStep("Unbind second slider", () => slider2.Current.UnbindFrom(slider.Current));
+        AddStep("Move mouse to 1/4 of first slider", () => InputManager.MoveMouseTo(new Vector2(slider.DrawRectangle.X + slider.DrawRectangle.Width * 0.25f, slider.DrawRectangle.Y + 10)));
+        AddStep("Click 1/4", () => InputManager.Click(MouseButton.Left));
+        AddAssert("First slider value is roughly 25", () => Precision.AlmostEquals(slider.Current.Value, 25f, 2f));
+        AddAssert("Second slider value is still roughly 75", () => Precision.AlmostEquals(slider2.Current.Value, 75f, 2f));
+    }
 }
