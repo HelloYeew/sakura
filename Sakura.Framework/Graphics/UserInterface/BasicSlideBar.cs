c9ebbd2a3a72dbe762edf34801b9fcb5f8b5b006
Phawit Pornwattanakul <me@helloyeew.dev>
2026-05-22 13:29:05 +0700

    Make slider bar able to change type of value

--- Sakura.Framework/Graphics/UserInterface/BasicSlideBar.cs
+++ Sakura.Framework/Graphics/UserInterface/BasicSlideBar.cs
@@ -2,21 +2,22 @@
 // See the LICENSE file for full license text.
 
 using System;
+using System.Numerics;
 using Sakura.Framework.Graphics.Colors;
 using Sakura.Framework.Graphics.Drawables;
 using Sakura.Framework.Graphics.Primitives;
 using Sakura.Framework.Input;
-using Sakura.Framework.Maths;
 using Sakura.Framework.Reactive;
+using Vector2 = Sakura.Framework.Maths.Vector2;
 
 namespace Sakura.Framework.Graphics.UserInterface;
 
-public class BasicSliderBar : Container
+public class BasicSliderBar<T> : Container where T : struct, INumber<T>
 {
-    public ReactiveFloat Current { get; } = new ReactiveFloat();
+    public Reactive<T> Current { get; } = new Reactive<T>(T.Zero);
 
-    public float MinValue { get; set; } = 0f;
-    public float MaxValue { get; set; } = 1f;
+    public T MinValue { get; set; }
+    public T MaxValue { get; set; }
 
     // TODO: Refactor framework color to central static class
     public Color BackgroundColor { get; set; } = Color.DarkGreen;
@@ -50,11 +51,14 @@ public class BasicSliderBar : Container
 
     private void updateSelection()
     {
-        float range = MaxValue - MinValue;
-        if (range <= 0)
-            return;
+        float min = float.CreateTruncating(MinValue);
+        float max = float.CreateTruncating(MaxValue);
+        float current = float.CreateTruncating(Current.Value);
 
-        float fill = Math.Clamp((Current.Value - MinValue) / range, 0f, 1f);
+        float range = max - min;
+        if (range <= 0) return;
+
+        float fill = Math.Clamp((current - min) / range, 0f, 1f);
         selection.Size = new Vector2(fill, 1);
     }
 
@@ -65,7 +69,11 @@ public class BasicSliderBar : Container
         float localX = screenSpaceMousePosition.X - DrawRectangle.X;
         float progress = Math.Clamp(localX / DrawRectangle.Width, 0f, 1f);
 
-        Current.Value = MinValue + progress * (MaxValue - MinValue);
+        float min = float.CreateTruncating(MinValue);
+        float max = float.CreateTruncating(MaxValue);
+        float newValue = min + progress * (max - min);
+
+        Current.Value = T.CreateTruncating(newValue);
     }
 
     public override bool OnMouseDown(MouseButtonEvent e)
