286114a1fd3cee5b70e43879815e60b22e3b086e
Phawit Pornwattanakul <me@helloyeew.dev>
2026-05-22 12:55:39 +0700

    Make active state test usable in headless

--- Sakura.Framework.Tests/Visuals/Platform/TestActiveState.cs
+++ Sakura.Framework.Tests/Visuals/Platform/TestActiveState.cs
@@ -33,8 +33,8 @@ public class TestActiveState : TestScene
         RelativeSizeAxes = Axes.Both
     };
 
-    [OneTimeSetUp]
-    public void SetUp()
+    [Test]
+    public void TestWindowActiveState()
     {
         AddStep("Add active box", () =>
         {
