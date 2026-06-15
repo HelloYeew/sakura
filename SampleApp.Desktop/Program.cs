af5400dd69ced5ab6592297a4644b86c01040207
Phawit Pornwattanakul <me@helloyeew.dev>
2026-06-15 12:02:48 +0700

    Add sample app to main repository

---
+++ SampleApp.Desktop/Program.cs
@@ -0,0 +1,20 @@
+﻿// This code is part of the Sakura framework project. Licensed under the MIT License.
+// See the LICENSE file for full license text.
+
+using System;
+using Sakura.Framework;
+using Sakura.Framework.Platform;
+
+namespace SampleApp.Desktop;
+
+class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        using (AppHost host = new DesktopAppHost("sample-app", new HostOptions()))
+        using (App app = new SampleAppApp())
+            host.Run(app);
+    }
+}
+
---
+++ SampleApp/SampleAppApp.cs
@@ -0,0 +1,34 @@
+﻿// This code is part of the Sakura framework project. Licensed under the MIT License.
+// See the LICENSE file for full license text.
+
+using Sakura.Framework;
+using Sakura.Framework.Graphics.Colors;
+using Sakura.Framework.Graphics.Drawables;
+using Sakura.Framework.Graphics.Primitives;
+using Sakura.Framework.Maths;
+
+namespace SampleApp;
+
+public class SampleAppApp : App
+{
+    private Box box = null!;
+
+    public override void Load()
+    {
+        base.Load();
+
+        Add(box = new Box()
+        {
+            Anchor = Anchor.Centre,
+            Origin = Anchor.Centre,
+            Size = new Vector2(100, 100),
+            Color = Color.LightPink
+        });
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        box.Rotation += (float)Clock.ElapsedFrameTime / 10;
+    }
+}
