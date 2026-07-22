297e0c213e1996c17c28e43118bdbc14ef2f631c
Phawit Pornwattanakul <me@helloyeew.dev>
2026-07-22 16:46:09 +0700

    Fix color picker SV area box size not respect the set size

--- Sakura.Framework/Graphics/UserInterface/ColorPicker.cs
+++ Sakura.Framework/Graphics/UserInterface/ColorPicker.cs
@@ -183,6 +183,11 @@ public abstract partial class ColorPicker : Container
     public override void LoadComplete()
     {
         base.LoadComplete();
+        
+        svArea.Size = SaturationValueAreaSize;
+        hueBar.Size = new Vector2(SaturationValueAreaSize.X, HueBarHeight);
+        if (hexInput != null)
+            hexInput.Size = new Vector2(SaturationValueAreaSize.X - (preview?.Size.X ?? 0) - Spacing, HueBarHeight + 8);
 
         // Derive the initial HSV from Current and paint everything once.
         deriveHsvFromCurrent(Current.Value);
